fcacae5542e772572793256d15af9c7a61bed4e5
Anthony Lamas <anthony21.lamas@gmail.com>
2024-08-07 20:07:03 -0700

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,37 +1,29 @@
-<?php
 
-/**
- * Conexión a base de datos de MySQL con PDO
- *
- * @author mroblesdev
- * @link https://github.com/mroblesdev/web_service_php
- * @license: MIT
- */
 
-class Conexion
-{
-    private $hostBd = 'localhost';
-    private $nombreBd = 'web_service';
-    private $usuarioBd = 'root';
-    private $passwordBd = 'password';
-    private $pdo; // Propiedad para almacenar el objeto PDO
-
-    public function __construct()
-    {
-        try {
-            // Crear una nueva conexión PDO
-            $this->pdo = new PDO("mysql:host=$this->hostBd;dbname=$this->nombreBd;charset=utf8", $this->usuarioBd, $this->passwordBd);
-
-            // Establecer el modo de errores de PDO a excepciones
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo "Error al conectar a la base de datos: " . $e->getMessage();
-            exit;
-        }
-    }
-
-    public function obtenerConexion()
-    {
-        return $this->pdo;
-    }
-}
+<?php 
+class Conexion 
+{ 
+    private $hostBd = 'localhost'; 
+    private $nombreBd = 'web_service'; 
+    private $usuarioBd = 'root'; 
+    private $pdo; // Propiedad para almacenar el objeto PDO 
+ 
+    public function __construct() 
+    { 
+        try { 
+            // Crear una nueva conexión PDO 
+            $this->pdo = new PDO("mysql:host=$this->hostBd;dbname=$this->nombreBd;charset=utf8", $this->usuarioBd); 
+ 
+            // Establecer el modo de errores de PDO a excepciones 
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        } catch (PDOException $e) { 
+            echo "Error al conectar a la base de datos: " . $e->getMessage(); 
+            exit; 
+        } 
+    } 
+ 
+    public function obtenerConexion() 
+    { 
+        return $this->pdo; 
+    } 
+} 
